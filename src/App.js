a60d2d1fa3633d98f7a2ae0ad18c0c8ee6c5450f
Misushka <maryna.seidel@yahoo.de>
2023-09-19 13:48:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
-
+//https://api.edamam.com/api/recipes/v2?type=public&q=cocktail&app_id=7f05e9a1&app_key=9d711c11df64e5935baca9496984fd61
 function App() {
+  const MY_ID = "7f05e9a1";
+  const MY_KEY = "9d711c11df64e5935baca9496984fd61";
+  useEffect(() => {
+    const getRecipe = async()=> {
+      const response = await fetch('https://api.edamam.com/api/recipes/v2?type=public&q=cocktail&app_id=${MY_ID}&app_key=${MY_KEY}');
+      const data = await response.json();
+      console.log(data);
+    }
+    getRecipe()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>RECIPES</p>
     </div>
   );
 }
-
 export default App;
